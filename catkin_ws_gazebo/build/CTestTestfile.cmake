19b3751baa0edb835a626f026a183f4efd97a40f
Burak Aksoy <burakaksoy20@gmail.com>
2021-08-31 12:27:06 -0400

    organize

--- catkin_ws_gazebo/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/burak/common/RESEARCH/AssistiveRobot-SimulationFiles/catkin_ws_gazebo/src
-# Build directory: /home/burak/common/RESEARCH/AssistiveRobot-SimulationFiles/catkin_ws_gazebo/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("lab_gazebo")
--- catkin_ws_gazebo/build/lab_gazebo/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/burak/common/RESEARCH/AssistiveRobot-SimulationFiles/catkin_ws_gazebo/src/lab_gazebo
-# Build directory: /home/burak/common/RESEARCH/AssistiveRobot-SimulationFiles/catkin_ws_gazebo/build/lab_gazebo
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
